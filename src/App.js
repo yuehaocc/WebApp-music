86b99de05cf189ee93d0f1c88c7abf0c6ae4002e
yuehao <879389355@qq.com>
2020-04-08 15:35:20 +0800

    add redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+
 import { HashRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import routes from './routes/index';
+
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 import { IconStyle } from './assets/iconfont/iconfont'
 import { GlobalStyle } from './style';
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
+
   );
 }
 
