eac5e7434897924c63ea6940808868ca1be78ba3
yuehao <879389355@qq.com>
2020-04-09 14:15:01 +0800

    add recommend song list

--- src/view/Rank/index.js
+++ src/view/Rank/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
+class CustomTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    this.textInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.textInput} />
+        <input type="button" value="Focus the text input" onClick={this.focusTextInput} />
+      </div>
+    )
+  }
+}
+
+class AutoFocusTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log('this.textInput.current', this.textInput.current)
+    this.textInput.current.focusTextInput()
+  }
+
+  render() {
+    return (<CustomTextInput ref={this.textInput} />)
+  }
+}
+
 function Rank(props) {
   return (
-    <div>Rank</div>
+    <div>
+      <h1>Rank</h1>
+      <AutoFocusTextInput></AutoFocusTextInput>
+    </div>
   )
 }
 
--- src/view/Recommend/index.js
+++ src/view/Recommend/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 
-function Recommend(props) {
+import Scroll from '../../baseComponents/scroll/index';
+import Slider from '../../components/slider';
+import RecommendList from '../../components/list';
+
+import { Content } from './style'
+
+function Recommend() {
+  const bannerList = [1, 2, 3, 4].map(item => {
+    return {
+      imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg"
+    }
+  })
+
+  const recommendList = [1, 2, 3, 4, 6, 7, 8, 9, 10].map((item, index) => {
+    return {
+      id: index,
+      picUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg",
+      playCount: index * Math.random(1000) * 1000,
+      name: `name ${index}`
+    }
+  })
+  console.log('bannerList', bannerList)
+  console.log('recommendList', recommendList)
+
+
   return (
-    <div>Recommend</div>
+    <Content>
+      <Scroll>
+        <div>
+          <Slider bannerList={bannerList}></Slider>
+          <RecommendList recommendList={recommendList}></RecommendList>
+        </div>
+      </Scroll>
+    </Content>
+
   )
 }
 
